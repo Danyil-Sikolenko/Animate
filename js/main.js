7cfa42a2db7de4fb31da418009c9986206e7bd82
Danyil-Sikolenko <banielsikolenko@gmail.com>
2024-11-06 14:28:33 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// $(function(){
+// $('.mavic').hover(
+//     function(){
+//         $('.mavic-img').addClass('mavic-on').removeClass('mavic-off');
+//     },
+//     function(){
+//         $('.mavic-img').removeClass('mavic-on').addClass('mavic-off');
+//     },
+// );
+// });
+
+
+/*переключашка img*/
+// что бы отключить анимацию нужно заюзать js and jQ
